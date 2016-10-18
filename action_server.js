79e3685ba4a66a3b0e1e0d5a74403c00fc738334
eddiexue <jxuedi@vip.qq.com>
2016-10-18 13:11:54 +0800

    bugfx

--- action_server.js
+++ action_server.js
@@ -11,7 +11,7 @@ var options = {
 };
 
 //为了测试，本地cache尽快更新; 把index.html改个名字试试
-var fileHandler = new nodestatic.Server( {cache: 1, indexFile: 'frontpage.html'} );
+var fileHandler = new nodeStatic.Server( {cache: 1, indexFile: 'frontpage.html'} );
 
 //启动简单文件服务器，到这里其实只负责把文件都拉回去
 var httpsServer = https.createServer(options, function(req, res){
