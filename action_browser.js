54d88de8e2979ac07208ff17e862e8444333744d
eddiexue <jxuedi@vip.qq.com>
2016-10-18 19:26:48 +0800

    add action of browser

--- action_browser.js
+++ action_browser.js
@@ -1,1 +1,54 @@
-console.log('hello world!')
+'use strict';
+
+var pcConfig = {
+  'iceServers': [
+    //{'url': 'stun:stun.l.google.com:19302'}
+    {
+      'url': 'turn:183.60.1.158:3478?transport=udp',
+      'credential': 'testpassword',
+      'username': 'testqq'
+    }
+  ]
+};
+
+// Set up audio and video regardless of what devices are present.
+var sdpConstraints = {
+  'mandatory': {
+    'OfferToReceiveAudio': true,
+    'OfferToReceiveVideo': true
+  }
+};
+
+/////////////////////////////////////////////
+
+var room = 'foo';
+// Could prompt for room name:
+room = prompt('Enter room name:');
+
+var socket = io.connect();
+
+if (room !== '') {
+  socket.emit('create or join', room);
+  console.log('Attempted to create or  join room', room);
+}
+
+socket.on('full', function(room) {
+  console.log('Room ' + room + ' is full');
+});
+
+socket.on('join', function (room){
+  console.log('Another peer made a request to join room ' + room);
+  console.log('This peer is the initiator of room ' + room + '!');
+  isChannelReady = true;
+});
+
+socket.on('joined', function(room) {
+  console.log('joined: ' + room);
+  isChannelReady = true;
+});
+
+socket.on('log', function(array) {
+  console.log.apply(console, array);
+});
+
+console.log('进行到这里，发生了什么？');
