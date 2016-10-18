83539424cf6b09e2ee57a46ea2b2200598feef75
eddiexue <jxuedi@vip.qq.com>
2016-10-19 00:10:41 +0800

    bugfix

--- action_server.js
+++ action_server.js
@@ -35,11 +35,37 @@ io.on('connection', function(socket){
         console.log(content);
     }
 
-    function printObj( obj )
+    function obj2string(o)
+    { 
+        var r=[]; 
+        if(typeof o=="string"){ 
+            return "\""+o.replace(/([\'\"\\])/g,"\\$1").replace(/(\n)/g,"\\n").replace(/(\r)/g,"\\r").replace(/(\t)/g,"\\t")+"\""; 
+        } 
+        if(typeof o=="object"){ 
+            if(!o.sort){ 
+                for(var i in o){ 
+                    r.push(i+":"+obj2string(o[i])); 
+                } 
+                if(!!document.all&&!/^\n?function\s*toString\(\)\s*\{\n?\s*\[native code\]\n?\s*\}\n?\s*$/.test(o.toString)){ 
+                    r.push("toString:"+o.toString.toString()); 
+                } 
+                r="{"+r.join()+"}"; 
+            }else{ 
+                for(var i=0;i<o.length;i++){ 
+                    r.push(obj2string(o[i])) 
+                } 
+                r="["+r.join()+"]"; 
+            }  
+            return r; 
+        }  
+        return o.toString(); 
+    } 
+
+    function printObj(obj)
     {
         for( var i in obj)
         {
-            log(i + '=' + obj[i].toString());
+            log(i + '=' + obj2string(obj[i]));
         }
     }
 
