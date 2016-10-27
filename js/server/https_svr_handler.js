2d96d49989fddaa8a192fa3d66f676086012b1f1
eddiexue <jxuedi@vip.qq.com>
2016-10-28 01:28:14 +0800

    butiful

--- js/server/https_svr_handler.js
+++ js/server/https_svr_handler.js
@@ -50,9 +50,9 @@ var pcConfig = {
 console.log('creating turnserver secure secret...');
 for(var i=0 ; i < pcConfig.iceServers.length; i++)
 {
-    console.log('url['+i+']='+pcConfig.iceServers[i].url);
-    console.log('username['+i+']='+pcConfig.iceServers[i].username);
-    console.log('credential['+i+']='+pcConfig.iceServers[i].credential);
+    console.log('url['+i+']=        '+pcConfig.iceServers[i].url);
+    console.log('username['+i+']=   '+pcConfig.iceServers[i].username);
+    console.log('credential['+i+']= '+pcConfig.iceServers[i].credential);
 }
 console.log('-----------------------------------');
 
