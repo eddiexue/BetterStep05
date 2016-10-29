97de1e34d3f46848db4665a629d9dd2a0b872e30
eddiexue <jxuedi@vip.qq.com>
2016-10-29 23:14:50 +0800

    debug

--- js/server/https_svr_handler.js
+++ js/server/https_svr_handler.js
@@ -1,7 +1,5 @@
 'use strict';
 
-
-//Todo1: 从turnserver目录下的配置文件里读取配置信息
 //Todo2: 修改SDP的内容，使其生成h264内容
 
 
@@ -70,7 +68,7 @@ function parseINIString(data)
 }  
 
 //从turnserver配置文件中读取？？
-var turnserver_conf = parseINIString(fs.readFileSync( workspaceDir+'/../../turnserver/conf/turnserver.conf' ));
+var turnserver_conf = parseINIString(fs.readFileSync( workspaceDir+'/../../turnserver/conf/turnserver.conf').toString());
 var turn_static_auth_secret = turnserver_conf.static-auth-secret;
 var turn_usercombo =  Math.floor(((new Date()).valueOf()/1000+86400*3650))+':'+'eddiexue';
 var turn_password = crypto.createHmac('sha1', turn_static_auth_secret).update(turn_usercombo).digest().toString('base64');
