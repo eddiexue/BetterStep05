556514ac553b8f405bacf399f3c2dce2b1584a03
eddiexue <jxuedi@vip.qq.com>
2016-10-29 23:07:36 +0800

    add new

--- js/server/https_svr_handler.js
+++ js/server/https_svr_handler.js
@@ -31,10 +31,53 @@ var secure_options = {
     cert: fs.readFileSync( workspaceDir+'/cert/certificate.pem')  
 };
 
+function parseINIString(data)
+{  
+    var regex = {  
+        section: /^\s*\s*([^]*)\s*\]\s*$/,
+        param: /^\s*([\w\.\-\_]+)\s*=\s*(.*?)\s*$/,  
+        comment: /^\s*#.*$/  
+    };  
+    var value = {};  
+    var lines = data.split(/\r\n|\r|\n/);  
+    var section = null;  
+    lines.forEach(function(line)
+    {  
+        if(regex.comment.test(line)){  
+            return;  
+        }
+        else if(regex.param.test(line))
+        {  
+            var match = line.match(regex.param);  
+            if(section){  
+                value[section][match[1]] = match[2];  
+            }else{  
+                value[match[1]] = match[2];  
+            }  
+        }
+        else if(regex.section.test(line))
+        {  
+            var match = line.match(regex.section);  
+            value[match[1]] = {};  
+            section = match[1];  
+        }
+        else if(line.length == 0 && section)
+        {  
+            section = null;  
+        };  
+    });  
+    return value;  
+}  
+
 //从turnserver配置文件中读取？？
-var turn_static_auth_secret = '4080218913';
+var turnserver_conf = parseINIString(fs.readFileSync( workspaceDir+'/../turnserver/conf/turnserver.conf' ));
+var turn_static_auth_secret = turnserver_conf.static-auth-secret;
 var turn_usercombo =  Math.floor(((new Date()).valueOf()/1000+86400*3650))+':'+'eddiexue';
 var turn_password = crypto.createHmac('sha1', turn_static_auth_secret).update(turn_usercombo).digest().toString('base64');
+console.log('turnserver_conf='+turnserver_conf);
+console.log(workspaceDir+'/../turnserver/conf/turnserver.conf');
+console.log('turn_static_auth_secret='+turn_static_auth_secret);
+
 
 var pcConfig = {
   'iceServers': [
